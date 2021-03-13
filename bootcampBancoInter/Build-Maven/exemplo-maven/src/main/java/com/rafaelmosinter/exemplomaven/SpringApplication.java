04c6c0e842a83c3a4b33ca7640705f4fc38f8c35
Rafaelmos <rafael.rjms0@gmail.com>
2021-03-13 07:52:46 -0300

    fundamentos

---
+++ bootcampBancoInter/Build-Maven/exemplo-maven/src/main/java/com/rafaelmosinter/exemplomaven/SpringApplication.java
@@ -0,0 +1,10 @@
+package com.rafaelmosinter.exemplomaven;
+
+public class SpringApplication {
+
+	public static void run(Class<ExemploMavenApplication> class1, String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
